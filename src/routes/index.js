2354f4855002df32adefd9ae4b980cb21dceeeef
thamiress-t <thamirest.ufpe@gmail.com>
2021-07-08 23:54:02 -0300

    procfile

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+//const express
+
+/*router.get("/", (request, response)=>{
+    response.status(200).json({
+        "titulo": "API de Filmes",
+        "version": "1.0.0",
+        "mensagem": "Criada com o proposito de ensinar deploy no Heroku com Node.js integração com Mongo db"
+    })
+})*/
