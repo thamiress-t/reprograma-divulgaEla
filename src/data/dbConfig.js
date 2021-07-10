fb7cc6c01c40fbce6c52e42fc1107f03abe66a14
thamiress-t <thamirest.ufpe@gmail.com>
2021-07-10 00:22:09 -0300

    adicionando uri

--- src/data/dbConfig.js
+++ src/data/dbConfig.js
@@ -2,16 +2,16 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 //Para o Atlas/ Heroku:
-/*const MONGO_URL = process.env.MONGODB_URI
+const MONGO_URL = process.env.MONGODB_URI
 const connect = () => {
     mongoose.connect(MONGO_URL,
         {
-*/
-
+/*
 //Para rodar localmente:
 const connect = () => {
     mongoose.connect(process.env.DATABASE_URL,
         {
+*/
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
